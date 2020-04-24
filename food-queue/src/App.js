76070114e5c94a1f4923a6300cac1b2c3da86cee
Deena57017 <Deena57017@gmail.com>
2020-04-24 22:17:39 +0700

    testfood3

--- food-queue/src/App.js
+++ food-queue/src/App.js
@@ -1,13 +1,46 @@
 import React ,{Component} from 'react';
 import FoodInput from './components/FoodInput'
 import FoodList from './components/FoodList'
+import uuid from "uuid";
 class App extends Component{
+    state={
+          items:[{id:1,name:"กะเพราไก่"},{id:2,name:"ผัดคะน้า"}],
+          id:uuid(),
+          items:"",
+          editItem:false
+    };
+  handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log("Submit Data");
+  }
+  handleChange=(e)=>{
+    this.setState({
+      item:e.target.value
+    })
+  }
+  clearList=()=>{
+    console.log("Clear Data");
+  }
+  handleDele=(e)=>{
+    console.log("Remove Item");
+  }
+  handleEdit=(e)=>{
+    console.log("Edit Item");
+  }
+
   render(){
+  
     return(
       <div className="container">
           <h2 align="center">Food Queue Application</h2>
         <div className="row">
-          <FoodInput/>
+          <FoodInput 
+          items={this.state.item}
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+          handleItem={this.handleItem}
+          />
+
         </div>
         <FoodList/>
         
@@ -15,4 +48,5 @@ class App extends Component{
     );
   }
 }
+
 export default App;
--- food-queue/src/components/FoodInput.js
+++ food-queue/src/components/FoodInput.js
@@ -2,14 +2,15 @@ import React ,{Component} from "react";
 
 export default class FoodInput extends Component{
     render(){
+        const {handleSubmit,item,handleChange,editItem} = this.props;
         return(
             <div className="card card-body my-3"> 
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="form-group">
                       <label>ป้อนรายการอาหาร</label>
-                      <input type="text" class="form-control" value=""/>
+                      <input type="text" class="form-control" value={item} onChange={handleChange}/>
                     </div>
-                    <input type="submit" class="btn btn-primary" value="บันทึกรายการ"/>
+                    <input type="submit" className="btn btn-primary" value="บันทึกรายการ"/>
 
                 </form>
             </div>
