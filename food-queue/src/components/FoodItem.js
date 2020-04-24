1a60bad5c2de000020b99738c880165eb9896843
Deena57017 <Deena57017@gmail.com>
2020-04-25 00:23:47 +0700

    test5

--- food-queue/src/components/FoodItem.js
+++ food-queue/src/components/FoodItem.js
@@ -2,8 +2,9 @@ import React ,{Component} from "react";
 
 export default class FoodItem extends Component{
     render(){
+        const {name}=this.props
         return(
-            <div> Hello FoodItem</div>
+        <div>{name}</div>
         )
     }
     
